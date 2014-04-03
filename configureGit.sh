10238063556895054ab98c7e21107171fac23175
Chris Wong <chriswong90012@yahoo.com>
2014-04-03 13:06:33 -0700

    added a documentation

--- configureGit.sh
+++ configureGit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script configures git whenever you start working
+# on multiple machines. Run it any time you decide to work
+# with git on a new computer
+
 git config --global user.name "Chris Wong"
 git config --global user.email "chriswong90012@yahoo.com"
 git config --global color.ui "auto"

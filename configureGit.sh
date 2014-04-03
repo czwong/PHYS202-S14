ef0e27211b326de25c31e5a22ecb4ef6607ce2ea
Chris Wong <chriswong90012@yahoo.com>
2014-04-03 12:55:06 -0700

    adding config and pledge

---
+++ configureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Chris Wong"
+git config --global user.email "chriswong90012@yahoo.com"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
